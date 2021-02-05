0d59b876c7c6e043b3d3b718ae59abeb3dba0520
Evan Stodddard <e.d.stoddard345@gmail.com>
2021-02-05 16:35:25 -0500

    revert to "fix Mobile View"

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -52,7 +52,7 @@ namespace ByteClubSite.Pages
                 .ToListAsync();
             foreach (var entry in UserLogins)
             {
-                if (entry.Password.ToString() == Password)
+                if (entry.Password == Password)
                 {
                     HttpContext.Session.SetString("username", Username);
                     HttpContext.Session.SetInt32("access", entry.hasEditorRights);
