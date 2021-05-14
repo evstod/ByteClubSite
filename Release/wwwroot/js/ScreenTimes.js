918c8fc42fbd5a15685a58ea7383d465f953b615
probablyatraitor <38697067+probablyatraitor@users.noreply.github.com>
2021-05-14 11:41:17 -0400

    framework-dependent release

---
+++ Release/wwwroot/js/ScreenTimes.js
@@ -0,0 +1,7 @@
+﻿//Need a formula to convert (TimeSpan)Agenda.StartTime into Date
+debugger;
+var starttimes;
+for (var i = 0; i < starttimes.length; i++) {
+    starttimes[i] = JSON.parse(@Model.AgendasList);
+    console.log(starttimes[i]);
+}
